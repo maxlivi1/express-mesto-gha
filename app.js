9ef860bc7c923e2871ff9e4e6e7a7800f161dc53
Max Limanskii <maxlivi@yandex.ru>
2023-07-02 17:10:02 +0300

    refactor: вернуть id пользователя

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  req.user = { _id: '5d8b8592978f8bd833ca8133' };
+  req.user = { _id: '649ea0a00c567ed792814472' };
   next();
 });
 
